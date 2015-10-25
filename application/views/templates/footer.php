c432cb0211ae1e023769b3bb4cbad914d44ab7f6
krishshanmukh <srikrishnashanmukh@gmail.com>
2015-10-25 13:16:15 +0530

    Adding CSS and JS files under asset folder

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,3 +1,8 @@
-	<p><em>&copy; 2015</em></p>	
+		<p><em>&copy; 2015</em></p>	
+
+		<script type="text/javascript" src="<?php echo base_url("assets/js/jQuery-2.1.4.js"); ?>"></script>
+		<script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
+
+		</div>
     </body>
 </html>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,7 +1,11 @@
 <html>
         <head>
                 <title><?php echo $title; ?></title>
+
+                <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/bootstrap.css");?>" />
         </head>
         <body>
+
+        	<div class="container">
 
                 <h1><?php echo $title; ?></h1>
